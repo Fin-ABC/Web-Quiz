8f760bdd13bc288dba4f6c1a11175a50788f065e
Fin-ABC <Ahmad.snesas@gmail.com>
2025-08-24 10:05:54 +0700

    Update detail page

--- app/style/js/script.js
+++ app/style/js/script.js
@@ -1,4 +1,22 @@
 const data_quiz = [
-  { id: "1", judul: "Test Level Pertama", author: "Fin", like: "12" },
-  { id: "2", judul: "Test Level Kelima", author: "Fin", like: "12" },
+  {
+    id: "1",
+    judul: "Test Level Pertama",
+    subjudul: "Ini adalah subjudul untuk level pertama",
+    jml_soal:"5",
+    author: "Fin",
+    like: "12",
+    deskripsi:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus ducimus fugiat blanditiis accusantium perspiciatis totam saepe enim sunt repudiandae, amet asperiores nisi quasi sapiente cupiditate dolorem placeat. Nostrum, eaque veniam! Expedita, corporis. Minima doloribus et id perferendis optio quos delectus minus tenetur aspernatur ut blanditiis ducimus, nesciunt modi quia laborum.",
+  },
+  {
+    id: "2",
+    judul: "Test Level Kelima",
+    subjudul: "Have fun playing this level",
+    jml_soal:"10",
+    author: "Fin",
+    like: "12",
+    deskripsi:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi beatae vel consequatur, quae delectus minima placeat id velit ducimus, sequi sapiente unde facilis sit recusandae officiis saepe! Est ab eaque voluptates doloribus possimus eveniet fuga repellat similique hic mollitia nesciunt molestias sit accusamus nemo ratione, adipisci expedita suscipit excepturi totam, natus quae reiciendis, velit cupiditate. Distinctio eius maxime harum tempore?",
+  },
 ];
